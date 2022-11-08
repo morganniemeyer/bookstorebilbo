fc8df8bdfd9d48341a03a87c1ab307959f4d1ea0
Morgan Niemeyer <malineniemeyer@gmail.com>
2022-11-07 18:45:26 -0600

    write test 2

---
+++ __tests__/book.test.js
@@ -0,0 +1,17 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('book routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+  it('should return a list of books', async () => {
+    const res = await request(app).get('/books');
+    expect(res.body.length).toEqual(10);
+  });
+  afterAll(() => {
+    pool.end();
+  });
+});
